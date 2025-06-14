31aec56a6a4000dd485a9e6d712a49c47f4ec4b7
Jhon Gregory Carrascal H. <Jhon Gregory Carrascal H.>
2025-06-13 21:00:22 -0500

    Environments

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig  = {
+    apiKey: "AIzaSyDVyLVLqzKiS-fs1jB5cjZowFmfOK8uBzI",
+  authDomain: "proyecto-aos-b500e.firebaseapp.com",
+  databaseURL: "https://proyecto-aos-b500e-default-rtdb.firebaseio.com",
+  projectId: "proyecto-aos-b500e",
+  storageBucket: "proyecto-aos-b500e.firebasestorage.app",
+  messagingSenderId: "234834556184",
+  appId: "1:234834556184:web:620d5e6ea29b5dd25dda6d"
+};
---
+++ y
@@ -0,0 +1,19 @@
+{
+  "indexes": [
+    {
+      "collectionGroup": "auditoria",
+      "queryScope": "COLLECTION",
+      "fields": [
+        {
+          "fieldPath": "metodoAutenticacion",
+          "order": "ASCENDING"
+        },
+        {
+          "fieldPath": "fechaRegistroServidor",
+          "order": "DESCENDING"
+        }
+      ]
+    }
+  ],
+  "fieldOverrides": []
+}
